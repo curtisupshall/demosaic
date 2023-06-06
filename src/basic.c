24e33005e7cd64413949a031dc53d1ff4ba8a3d3
Kjartan <Kjartan.Einarsson@quartech.com>
2023-06-05 19:34:35 -0700

    back to basics, 'Hellow WOrld

--- src/basic.c
+++ src/basic.c
@@ -1,10 +1,3 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-
-#define DEBUG
-#ifdef DEBUG
-# define DEBUG_PRINT(x) printf x
-#else
-# define DEBUG_PRINT(x) do {} while (0)
-#endif
+void main (void) {
+    printf("Hello World!\n");
+}
