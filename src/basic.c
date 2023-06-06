1822a687aeb9da01a0afa66d5e38b14edc76ecd2
Kjartan <Kjartan.Einarsson@quartech.com>
2023-06-05 19:17:39 -0700

    test using arm code

--- src/basic.c
+++ src/basic.c
@@ -1,4 +1,9 @@
 int add( register int a, register int b){
-  register int sum = a + b;
-  return sum ;
+  register int sum ;
+  __asm__ __volatile__ (
+                        "qadd\t %0, %1, %2\n"
+                        : "=r" ( sum )
+                        : "r" ( a ) , "r" ( b )
+                        );
+  return( sum );
 }
