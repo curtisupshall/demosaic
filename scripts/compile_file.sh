c2c584d4a5dc55991b99ab766d72fa159991aa9e
Kjartan <Kjartan.Einarsson@quartech.com>
2023-06-05 18:47:57 -0700

    remove git upload

--- scripts/compile_file.sh
+++ scripts/compile_file.sh
@@ -10,7 +10,4 @@ process_id=$!
 /opt/arm/4.3.2/bin/arm-linux-gcc -mfloat-abi=softfp -mfpu=neon -static -O3 -S /tmp/seng440/basic.c -o ./src/compile/basic.exe
 wait $process_id
 
-git add ./src/compile/basic.exe
-git commit -m "Compiled basic.c to basic.exe"
-git push origin master
 EOF
