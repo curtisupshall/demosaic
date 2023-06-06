5df7a1c56601f5e721bde90ff947aa8c755fec46
Kjartan <Kjartan.Einarsson@quartech.com>
2023-06-05 18:50:54 -0700

    switch to .s

--- scripts/compile_file.sh
+++ scripts/compile_file.sh
@@ -5,9 +5,8 @@ echo Copying files to /tmp
 mkdir -p /tmp/seng440/
 cp ./src/basic.c /tmp/seng440/
 
-echo Compile basic.c to basic.exe
+echo Compile basic.c to basic.s
 process_id=$!
-/opt/arm/4.3.2/bin/arm-linux-gcc -mfloat-abi=softfp -mfpu=neon -static -O3 -S /tmp/seng440/basic.c -o ./src/compile/basic.exe
+/opt/arm/4.3.2/bin/arm-linux-gcc -mfloat-abi=softfp -mfpu=neon -static -O3 -S /tmp/seng440/basic.c -o ./src/compile/basic.s
 wait $process_id
-
-EOF
+l
