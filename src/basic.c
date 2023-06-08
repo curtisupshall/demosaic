59790be4a90600075662bce46b28298ea0bfaff6
Kjartan <Kjartan.Einarsson@quartech.com>
2023-06-08 12:58:28 -0700

    test

--- src/basic.c
+++ src/basic.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
-int main(void) {
-int a = 2;
-int b = 5;
-int c = 0;
-	
-c = a+b;
-printf("%d \n", c);
 
-return c;
+int main(void) {
+    int a = 2;
+    int b = 5;
+    int c = 0;
+        
+    c = a+b;
+    printf("%d \n", c);
+
+    return c;
 }
