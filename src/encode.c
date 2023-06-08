183ddfadb4af8467c5f43bd2dc3f815f1b7843f8
Kjartan <Kjartan.Einarsson@quartech.com>
2023-06-08 13:08:43 -0700

    encode update

--- src/encode.c
+++ src/encode.c
@@ -3,7 +3,8 @@
 #include <stdlib.h>
 
 #pragma pack(push, 1)
-typedef struct {
+typedef struct
+{
     uint16_t type;
     uint32_t size;
     uint16_t reserved1;
@@ -11,7 +12,8 @@ typedef struct {
     uint32_t offset;
 } BMPHeader;
 
-typedef struct {
+typedef struct
+{
     uint32_t size;
     int32_t width;
     int32_t height;
@@ -26,9 +28,11 @@ typedef struct {
 } BMPInfoHeader;
 #pragma pack(pop)
 
-int main() {
+int main()
+{
     FILE *fp = fopen("cow.bmp", "rb");
-    if (!fp) {
+    if (!fp)
+    {
         printf("Error opening file.\n");
         return 1;
     }
@@ -41,14 +45,16 @@ int main() {
     fread(&infoHeader, sizeof(BMPInfoHeader), 1, fp);
 
     // Check if it's a valid BMP file
-    if (header.type != 0x4D42) {
+    if (header.type != 0x4D42)
+    {
         printf("Invalid BMP file.\n");
         fclose(fp);
         return 1;
     }
 
     // Check if it's a 24-bit BMP file
-    if (infoHeader.bitDepth != 24) {
+    if (infoHeader.bitDepth != 24)
+    {
         printf("Unsupported bit depth. Only 24-bit BMP is supported.\n");
         fclose(fp);
         return 1;
@@ -58,16 +64,22 @@ int main() {
     uint32_t rowSize = ((infoHeader.width * 3 + 3) & ~3);
 
     // Allocate memory for the pixel data
-    uint8_t *pixels = (uint8_t*)malloc(rowSize * infoHeader.height);
+    uint8_t *pixels = (uint8_t *)malloc(rowSize * infoHeader.height);
 
     // Read the pixel data
     fread(pixels, rowSize * infoHeader.height, 1, fp);
 
     fclose(fp);
 
+    int y;
+    int x;
+    int z;
+
     // Access pixel values
-    for (int y = 0; y < infoHeader.height; y++) {
-        for (int x = 0; x < infoHeader.width; x ++) {
+    for (y = 0; y < infoHeader.height; y++)
+    {
+        for (x = 0; x < infoHeader.width; x++)
+        {
             uint8_t *px = &pixels[y * rowSize + x * 3];
             // printf("Pixel (%d, %d): R=%d, G=%d, B=%d\n", x, y, px[2], px[1], px[0]);
 
@@ -82,23 +94,25 @@ int main() {
             px[0] = 0; // Blue channel
         }
         y++;
-        for (int x = 0; x < infoHeader.width; x ++) {
-            uint8_t *px = &pixels[y * rowSize + x * 3];
+        for (z = 0; z < infoHeader.width; z++)
+        {
+            uint8_t *px = &pixels[y * rowSize + z * 3];
             // Blue pixel
-            px[2] = 0; 
+            px[2] = 0;
             px[1] = 0;
 
-            x++;
-            px = &pixels[y * rowSize + x * 3];
+            z++;
+            px = &pixels[y * rowSize + z * 3];
             // Green pixel
-            px[2] = 0; 
+            px[2] = 0;
             px[0] = 0;
         }
     }
 
     // Create a new output file to write the modified image
     FILE *outFp = fopen("output/encoded.bmp", "wb");
-    if (!outFp) {
+    if (!outFp)
+    {
         printf("Error creating output file.\n");
         free(pixels);
         return 1;
