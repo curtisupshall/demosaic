94ccb51f84f27369ad9bf9069a68a07e90af91a8
Kjartan <Kjartan.Einarsson@quartech.com>
2023-07-11 14:38:16 -0700

    swap location

--- scripts/compile_file.sh
+++ scripts/compile_file.sh
@@ -3,11 +3,11 @@
 echo Copying files to /tmp
 
 mkdir -p /tmp/seng440/
-cp ./src/decode.c /tmp/seng440/
+cp ./decode.c /tmp/seng440/
 
 echo Compile decode.c to decode.exe
 process_id=$!
-/opt/arm/4.3.2/bin/arm-linux-gcc -mfloat-abi=softfp -mfpu=neon -static -O3 -S /tmp/seng440/decode.c -o ./src/compile/decode.exe
+/opt/arm/4.3.2/bin/arm-linux-gcc -mfloat-abi=softfp -mfpu=neon -static -O3 -S /tmp/seng440/decode.c -o ./decode.exe
 wait $process_id
 
 echo COMPILE COMPLETE
