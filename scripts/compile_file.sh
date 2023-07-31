51574b91cc62465054820912c0dd996704d0848a
Kjartan <Kjartan.Einarsson@quartech.com>
2023-07-31 13:22:29 -0700

    change folder name

--- scripts/compile_file.sh
+++ scripts/compile_file.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 echo Copying files to /tmp
-mkdir -p /tmp/seng440/
-cp ./src/decode.c /tmp/seng440/
+mkdir -p /tmp/demosaic440/
+cp ./src/decode.c /tmp/demosaic440/
 
 echo copy files for arm device to /demosaic_arm
 mkdir -p ./demosaic_arm/
@@ -10,7 +10,7 @@ cp ./data/encoded/cow.bmp ./demosaic_arm/encoded.bmp
 
 echo Compile decode.c to decode.exe
 process_id=$!
-/opt/arm/4.3.2/bin/arm-linux-gcc -mfloat-abi=softfp -mfpu=neon -static -O3 /tmp/seng440/decode.c -o ./demosaic_arm/decode.exe
+/opt/arm/4.3.2/bin/arm-linux-gcc -mfloat-abi=softfp -mfpu=neon -static -O3 /tmp/demosaic440/decode.c -o ./demosaic_arm/decode.exe
 wait $process_id
 
 
