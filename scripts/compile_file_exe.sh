5f736a96b588f4f144acc234e6a4b7f740da5070
Kjartan <Kjartan.Einarsson@quartech.com>
2023-08-15 15:38:21 -0700

    compile location

--- scripts/compile_file_exe.sh
+++ scripts/compile_file_exe.sh
@@ -6,14 +6,15 @@ filename=${1?param missing - file name.}
 
 echo Copying files to /tmp
 
-mkdir -p /tmp/seng440/
+mkdir -p /tmp/seng440_demosaic/
 
-cp ./src/$filename.c /tmp/seng440/
+cp ./src/$filename.c /tmp/seng440_demosaic/
 
 echo Compile $filename.c to $filename.exe
 process_id=$!
-#/opt/arm/4.3.2/bin/arm-linux-gcc -mfloat-abi=softfp -mfpu=neon -static -O3  /tmp/seng440/$filename.c -o ./src/compile/$filename.exe
-/opt/arm/4.3.2/bin/arm-linux-gcc  /tmp/seng440/$filename.c -o ./src/compile/$filename.exe
+#/opt/arm/4.3.2/bin/arm-linux-gcc -mfloat-abi=softfp -mfpu=neon -static -O3  /tmp/seng440_demosaic/$filename.c -o ./src/compile/$filename.exe
+/opt/arm/4.3.2/bin/arm-linux-gcc  /tmp/seng440_demosaic/$filename.c -o ./src/compile/$filename.exe
+/opt/arm/4.3.2/bin/arm-linux-gcc  /tmp/seng440_demosaic/$filename.c -o /tmp/seng440_demosaic/$filename.exe
 wait $process_id
 
 echo COMPILE COMPLETE
