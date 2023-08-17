538ab5388f853b798dcac9931193504c35f1f75e
Kjartan <Kjartan.Einarsson@quartech.com>
2023-08-17 13:54:03 -0700

    add v1 to prev_v

--- scripts/compile_file.sh
+++ scripts/compile_file.sh
@@ -2,16 +2,16 @@
 
 echo Copying files to /tmp
 mkdir -p /tmp/demosaic440/
-cp ./src/decode.c /tmp/demosaic440/
+cp ./src/prev_version/decode_v1.c /tmp/demosaic440/decode.c
 
 echo copy files for arm device to /demosaic_arm
 mkdir -p ./demosaic_arm/
-cp ./data/encoded/$1.bmp ./demosaic_arm/encoded.bmp
+cp ./data/encoded/* ./demosaic_arm/*
 
 echo Compile decode.c to decode.exe
 process_id=$!
-gcc /tmp/demosaic440/decode.c -g -o ./demosaic_arm/metric.exe
-gcc /tmp/demosaic440/decode.c -S -o ./demosaic_arm/assembly.s
+# gcc /tmp/demosaic440/decode.c -g -o ./demosaic_arm/metric.exe
+# gcc /tmp/demosaic440/decode.c -S -o ./demosaic_arm/assembly.s
 /opt/arm/4.3.2/bin/arm-linux-gcc -mfloat-abi=softfp -mfpu=neon -static -O3 /tmp/demosaic440/decode.c -pg -o ./demosaic_arm/decode_neon.exe
 wait $process_id
 
